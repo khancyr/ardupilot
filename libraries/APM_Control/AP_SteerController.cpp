35d82b33461a5c1e039f4b7b766dd826c1ab6021
Pierre Kancir <pierre.kancir@retdtechfrance.com>
2017-03-16 14:06:40 +0100

    APM_Control: temp disable reverse in steering rate control DONT MERGE

--- libraries/APM_Control/AP_SteerController.cpp
+++ libraries/APM_Control/AP_SteerController.cpp
@@ -143,9 +143,9 @@ int32_t AP_SteerController::get_steering_out_rate(float desired_rate)
 	// Calculate the steering rate error (deg/sec) and apply gain scaler
     // We do this in earth frame to allow for rover leaning over in hard corners
     float yaw_rate_earth = ToDeg(_ahrs.get_yaw_rate_earth());
-    if (_reverse) {
-        yaw_rate_earth = wrap_180(180 + yaw_rate_earth);
-    }
+    //if (_reverse) {
+    //    yaw_rate_earth = wrap_180(180 + yaw_rate_earth);
+    //}
     float rate_error = (desired_rate - yaw_rate_earth) * scaler;
 	
 	// Calculate equivalent gains so that values for K_P and K_I can be taken across from the old PID law
